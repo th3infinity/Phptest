49494dfafd906abcacc4408245607f415f834336
lucasvog <lucasvog@users.noreply.github.com>
2015-10-06 16:52:32 +0200

    Create home.php to host all the things

---
+++ home.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+</head>
+<<body>
+<?php
+
+if(!isset($_SESSION["session_ID"])) {
+	$SESSION["session_ID"] = rand(10000, 200000);
+}
+echo $SESSION["session_ID"];
+unset($SESSION["session_ID"]);
+ ?>
+
+</body>
+
+</html>
